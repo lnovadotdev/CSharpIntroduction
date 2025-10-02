95a2d63b9e069af410e37c16721ba2cfe5d0b62e
Luna Nova <sam@samhart.co.uk>
2025-10-02 17:11:55 +0100

    Add member overload

--- AdditionOverload/AdditionOverload.cs
+++ AdditionOverload/AdditionOverload.cs
@@ -1,14 +1,14 @@
 ﻿internal class AdditionOverload
 {
-    private static void Main(string[] args)
+    static int Add(int a, int b) => a + b;
+    static float Add(float a, float b) => a + b;
+    public static void Main(string[] args)
     {
-        static int Add(int a, int b) => a + b;
-
-        Console.Write("Enter the first integer: ");
-        int firstInteger = int.Parse(Console.ReadLine()!);
-        Console.Write("Enter the second integer: ");
-        int secondInteger = int.Parse(Console.ReadLine()!);
-        int sum = Add(firstInteger, secondInteger);
-        Console.WriteLine("The sum of the two integers is: " + sum);
+        Console.Write("Enter the first number: ");
+        float firstNumber = float.Parse(Console.ReadLine()!);
+        Console.Write("Enter the second number: ");
+        float secondNumber = float.Parse(Console.ReadLine()!);
+        float sum = Add(firstNumber, secondNumber);
+        Console.WriteLine("The sum of the two numbers is: " + sum);
     }
 }
