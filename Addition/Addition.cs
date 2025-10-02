9422413a0964d0e5274d1786126ad8a6758181f5
Luna Nova <sam@samhart.co.uk>
2025-10-02 16:25:40 +0100

    Complete Addition project

--- Addition/Addition.cs
+++ Addition/Addition.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int Add (int a, int b) => a + b;
+
+Console.Write("Enter the first integer: ");
+int firstInteger = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the second integer: ");
+int secondInteger = int.Parse(Console.ReadLine()!);
+int sum = Add(firstInteger, secondInteger);
+Console.WriteLine("The sum of the two integers is: " + sum);
