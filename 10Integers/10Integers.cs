4982a7188f092962200955abffaea84e591d2fb8
Luna Nova <sam@samhart.co.uk>
2025-10-02 16:17:42 +0100

    Add reverse order printing code
    Add code to prin the integers input by the user in reverse order of their input (last input first, first input last)

--- 10Integers/10Integers.cs
+++ 10Integers/10Integers.cs
@@ -1,13 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int highestInput = 0;
 int number = 0;
+int[] numbers = new int[10];
 for (int i = 0; i < 10; i++)
 {
     Console.Write("Enter an integer: ");
     number = int.Parse(Console.ReadLine()!);
+    numbers[i] = number;
     if (i == 0 || number > highestInput)
     {
         highestInput = number;
     }
 }
 Console.WriteLine("The highest integer is: " + highestInput);
+Console.Write("The integers you entered (in reverse order) are: ");
+for (int i = 9; i >= 0; i--)
+{
+    Console.Write(numbers[i] + " ");
+}
