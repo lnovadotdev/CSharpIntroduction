ba0e46a825e86cc5de4702ee69e6a3b8601b69fc
Luna Nova <sam@samhart.co.uk>
2025-10-02 16:03:07 +0100

    Complete rectangle project

--- Rectangle/Rectangle.cs
+++ Rectangle/Rectangle.cs
@@ -1,1 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter the width of the rectangle: ");
+float rectangleWidth = float.Parse(Console.ReadLine()!);
+Console.Write("Enter the height of the rectangle: ");
+float rectangleHeight = float.Parse(Console.ReadLine()!);
+float rectangleArea = rectangleWidth * rectangleHeight;
+float rectanglePerimeter = 2 * (rectangleWidth + rectangleHeight);
+Console.WriteLine("The area of the rectangle is: " + rectangleArea);
+Console.WriteLine("The perimeter of the rectangle is: " + rectanglePerimeter);
