e582822594d27a8ce71c903a46b7d2d505ec9b70
Luna Nova <sam@samhart.co.uk>
2025-10-02 17:17:28 +0100

    Complete GuessingGame project

--- GuessingGame/GuessingGame.cs
+++ GuessingGame/GuessingGame.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int rand = new Random().Next(1, 101);
+int guess;
+
+do
+{
+    Console.Write("Enter a number between 1 and 100: ");
+    guess = int.Parse(Console.ReadLine()!);
+    if (guess < rand)
+    {
+        Console.WriteLine("Too low! Try again.");
+    }
+    else if (guess > rand)
+    {
+        Console.WriteLine("Too high! Try again.");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations! You guessed the number!");
+        break;
+    }
+} while (guess != rand);
