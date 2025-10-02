fa49e094801c93a3fa8e9a32684aa5f85e3ab42f
Luna Nova <sam@samhart.co.uk>
2025-10-02 16:07:39 +0100

    Complete HighestInteger project

--- HighestInteger/HighestInteger.cs
+++ HighestInteger/HighestInteger.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int highestInput = 0;
+int number = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Enter an integer: ");
+    number = int.Parse(Console.ReadLine()!);
+    if (i == 0 || number > highestInput)
+    {
+        highestInput = number;
+    }
+}
