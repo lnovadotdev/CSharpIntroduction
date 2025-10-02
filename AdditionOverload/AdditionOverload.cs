671e727dd7c009ae22e443ebe22aa66e5a7791db
Luna Nova <sam@samhart.co.uk>
2025-10-02 17:08:59 +0100

    Convert AdditionOverload.cs
    Converting AdditionOverload.cs to a Main style program to allow for member overloading

--- AdditionOverload/AdditionOverload.cs
+++ AdditionOverload/AdditionOverload.cs
@@ -1,9 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-int Add(int a, int b) => a + b;
+﻿internal class AdditionOverload
+{
+    private static void Main(string[] args)
+    {
+        static int Add(int a, int b) => a + b;
 
-Console.Write("Enter the first integer: ");
-int firstInteger = int.Parse(Console.ReadLine()!);
-Console.Write("Enter the second integer: ");
-int secondInteger = int.Parse(Console.ReadLine()!);
-int sum = Add(firstInteger, secondInteger);
-Console.WriteLine("The sum of the two integers is: " + sum);
+        Console.Write("Enter the first integer: ");
+        int firstInteger = int.Parse(Console.ReadLine()!);
+        Console.Write("Enter the second integer: ");
+        int secondInteger = int.Parse(Console.ReadLine()!);
+        int sum = Add(firstInteger, secondInteger);
+        Console.WriteLine("The sum of the two integers is: " + sum);
+    }
+}
